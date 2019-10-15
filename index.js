a32141b18fc8b24192cd1af8dd700ced94bcac43
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-10-15 21:22:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 class Formatter {
   //add static methods here
   static capitalize (str){
-    str = str.charAt(0).toUpperCase()+str.slice(1);
+    return str.charAt(0).toUpperCase()+str.slice(1);
   }
   static sanitize(str) {
-    str = str.replace(/[^A-Za-z0-9-']+/g, '')
+    return str.replace(/[^A-Za-z0-9-']+/g, '')
   }
   static titleize(str){
     let bannedWords = ['a','an','but','of','and','for','at','by','from'];
