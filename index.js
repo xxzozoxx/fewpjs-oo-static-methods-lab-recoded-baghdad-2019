c4b26002f4dc9b63af6e30fd31abd31976a57bd7
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-10-15 21:23:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Formatter {
     return str.charAt(0).toUpperCase()+str.slice(1);
   }
   static sanitize(str) {
-    return str.replace(/[^A-Za-z0-9-']+/g, '') /[^A-Za-z0-9 '-]/g, '' 
+    return str.replace(/[^A-Za-z0-9 '-]/g, '')  
   }
   static titleize(str){
     let bannedWords = ['a','an','but','of','and','for','at','by','from'];
