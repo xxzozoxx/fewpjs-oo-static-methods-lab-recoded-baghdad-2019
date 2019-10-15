7a3bbbe520010547d0da7d8e4efd98ce93e96b1f
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-10-15 21:24:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class Formatter {
   //add static methods here
   static capitalize (str){
-    return str.charAt(0).toUpperCase()+str.slice(1);
+    return str.charAt(0).toUpperCase() + str.slice(1);
   }
   static sanitize(str) {
     return str.replace(/[^A-Za-z0-9 '-]/g, '')  
